1fbaf15970dd6bdd6002f53e24e347bd0bee212a
SamuelCagnani <samuelcagnani@gmail.com>
2026-03-03 11:21:46 -0300

    docs(readme): add tree limit

--- ia/Node.java
+++ ia/Node.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 
 public class Node {
 
+    // k = 10;
+
     private char origem;
     private char destino;
     private boolean vez; // 0 - preto e 1 - branco
     private char[][] matriz;
     private ArrayList<Node> filhos;
+
+
 
 }
